44f9e175528d266077b609ebd1a765cfb855f944
09765432sx <anushri.garg.cse.2023@miet.ac.in>
2026-02-23 20:05:19 +0530

    added all files

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,31 +1,31 @@
 dic={
-#     344:'yogesh',
-#     345:'abhishek',
-#     346:'satyarth',
-#     347:'harry',
-#     348:'aman',
-#     349:'mohit',
-#     350:'vaibhav'
+    344:'yogesh',
+    345:'abhishek',
+    346:'satyarth',
+    347:'harry',
+    348:'aman',
+    349:'mohit',
+    350:'vaibhav'
 
-# }
+}
 
-# print(dic[347])
+print(dic[347])
 
-# info={ 'name':'yogesh', 'age':19, 'is_student':True, 'height':5.9, 'age':19 }
-# print(info)
+info={ 'name':'yogesh', 'age':19, 'is_student':True, 'height':5.9, 'age':19 }
+print(info)
 
-# # print(info['name'])       # to access the value of a key in a dictionary we can use the key name inside square brackets
-# # print(info.get('age'))    # to access the value of a key in a dictionary we can also use the get() method of the dictionary, which takes the key name as an argument and returns the value of that key. If the key is not present in the dictionary, it will return None instead of raising an error like the square bracket method.
-# # print(info.keys())        # to get all the keys of a dictionary we can use the keys() method of the dictionary, which returns a view object that contains all the keys of the dictionary.
-# # print(info.values())      # to get all the values of a dictionary we can use the values
+print(info['name'])       # to access the value of a key in a dictionary we can use the key name inside square brackets
+print(info.get('age'))    # to access the value of a key in a dictionary we can also use the get() method of the dictionary, which takes the key name as an argument and returns the value of that key. If the key is not present in the dictionary, it will return None instead of raising an error like the square bracket method.
+print(info.keys())        # to get all the keys of a dictionary we can use the keys() method of the dictionary, which returns a view object that contains all the keys of the dictionary.
+print(info.values())      # to get all the values of a dictionary we can use the values
 
-# for keys in info.keys():
-#     # print(info[keys])
+for keys in info.keys():
+    print(info[keys])
 
-#     # print(f'the value corresponding to the key {keys} is {info[keys]}')
+    print(f'the value corresponding to the key {keys} is {info[keys]}')
 
     
-#     print(info.items())       # to get all the key-value pairs of a dictionary we can use the items() method of the dictionary, which returns a view object that contains all the key-value pairs of the dictionary as tuples. We can also use a for loop to iterate through the items of the dictionary and print them in a formatted way as shown above.
+    print(info.items())       # to get all the key-value pairs of a dictionary we can use the items() method of the dictionary, which returns a view object that contains all the key-value pairs of the dictionary as tuples. We can also use a for loop to iterate through the items of the dictionary and print them in a formatted way as shown above.
 
-# for key,value in info.items():
-#     print(f'the value corresponding to the key {key} is {value}')
+for key,value in info.items():
+    print(f'the value corresponding to the key {key} is {value}')
