854b1dd35017430eefa2e1c6070c4b7a60e3dc68
09765432sx <anushri.garg.cse.2023@miet.ac.in>
2026-02-24 23:46:29 +0530

    completed lec 50

---
+++ file_I_o.py
@@ -0,0 +1,11 @@
+# f = open('myfile.txt', 'r')
+# text=f.read()
+# print(text)
+# f.close()
+
+# f=open('myfile.txt', 'a')
+# f.write('Hello! World')
+# f.close()
+
+# with open('myfile.txt', "a") as f:
+#     f.write('hi i am studying python')
