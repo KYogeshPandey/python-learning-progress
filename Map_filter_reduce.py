5447612217547fd24662ab37203a374c7742b41c
09765432sx <anushri.garg.cse.2023@miet.ac.in>
2026-02-26 16:31:28 +0530

    complted till lec-55

--- Map_filter_reduce.py
+++ Map_filter_reduce.py
@@ -1,6 +1,26 @@
-l=[1,2,4,5,3,6,4]
-def cube(x):
-    return x*x*x
+ #Map
 
-newl= list(map(cube, l))
+l=[1,2,4,5,3,6,4]
+# def cube(x):
+#     return x*x*x
+
+newl= list(map(lambda x: x*x*x, l))
 print(newl)
+
+#Filter
+
+def filter_function(a):
+    return a>4
+
+newnewl=list(filter(filter_function, newl))
+print(newnewl)
+
+#reduce
+    
+from functools import reduce
+
+numbers = [1,2,3,4,5]
+
+sum = reduce(lambda x,y: x+y, numbers)
+
+print(sum)
